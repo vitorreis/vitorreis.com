8c274f8835c53b3cdcc982b3ad062a577184abd9
Vitor Reis <hi@vitorreis.com>
2019-05-20 21:24:53 +0200

    enable google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-140524142-1`,
       },
     },
     `gatsby-plugin-feed`,
